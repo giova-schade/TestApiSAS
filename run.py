698f6ea0188db7b27281aebea064035579471745
Giovanni Schade <gio.schade.c@gmail.com>
2022-12-05 12:57:30 -0300

    update

--- run.py
+++ run.py
@@ -5,7 +5,7 @@ def geTgt(origen,user,password):
     data = {'username': user, 'password': password}
     headers = {"Content-Type": "application/x-www-form-urlencoded"}
     urlTicket = origen+'/SASLogon/v1/tickets'
-    response = requests.post(urlTicket, headers=headers, params=data)    
+    response = requests.post(urlTicket, headers=headers, params=data, verify = False)    
     if(response.status_code == 201 or response.status_code == 200 ):
         tgt = response.headers['Location'].split('/')[len(response.headers['Location'].split('/'))-1]
         return tgt , response.status_code 
@@ -17,7 +17,7 @@ def getTkt(origen, tgt , statuscode):
         data = {'service': origen+'/SASStoredProcess/do'}
         headers = {"Content-Type": "application/x-www-form-urlencoded"}
         urlTicket = origen+'/SASLogon/v1/tickets/'+tgt
-        response = requests.post(urlTicket, headers=headers, params=data)
+        response = requests.post(urlTicket, headers=headers, params=data, verify = False)
         if (response.status_code == 200 or response.status_code == 201):
             ticket = response.text
             return ticket , response.status_code
@@ -31,7 +31,7 @@ def executeProgram(origen, ticket, statuscode , program):
         data = {'_program': program}
         headers = {"Content-Type": "application/json"}
         urlTicket = origen+'/SASStoredProcess/do?ticket='+ticket
-        response = requests.post(urlTicket, headers=headers, params=data)
+        response = requests.post(urlTicket, headers=headers, params=data, verify = False)
         if (response.status_code == 200 or response.status_code == 201):
             ticket = response.text
             return ticket , response.status_code
